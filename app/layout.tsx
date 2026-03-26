8b56b8edeb6e21c546f3968f36c4d72e963ce655
(주)피드백1 <feedback1@jupideubaeg1ui-iMac.local>
2026-03-26 14:25:47 +0900

    태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,24 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1500184178393418');
+fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
         {/* Danggeun Market Code */}
         <script src="https://karrot-pixel.business.daangn.com/karrot-pixel.js" />
         <script
@@ -38,7 +56,15 @@ export default function RootLayout({
         />
         {/* End Danggeun Market Code */}
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1500184178393418&ev=PageView&noscript=1" alt="" />',
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
